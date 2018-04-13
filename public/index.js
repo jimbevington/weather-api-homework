d9389a27f420b9e21f430d9a5b301763f982aa54
Jim Bevington <jim.bevington@hotmail.com>
2018-04-13 12:19:54 +0100

    added makeRequest method, glasgow Weather API Url

--- public/index.js
+++ public/index.js
@@ -1,4 +1,27 @@
+/// GENERIC
+
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();   //  a request object
+
+  request.open('GET', url); // make the request
+
+  request.addEventListener('load', callback)  //  when response, exec callback
+
+  request.send();
+};
+
+const getWeatherData = function(){
+
+  if(this.status !== 200) return;   // Check if response has worked
+  debugger;
+
+}
+
+
 var app = function(){
+
+  const glasgowWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=3333231&APPID=d7d64ab41161dd5f312ccbe208418afe";
+  makeRequest(glasgowWeatherURL, getWeatherData);
 
 }
 
