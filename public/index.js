a45bdfae668698ba036139a1803df9972e2d9fd1
Jim Bevington <jim.bevington@hotmail.com>
2018-04-13 13:32:20 +0100

    finished rough display of weather data from displayCurrentWeather. added test code for getting current Date, Time and generating Audio

--- public/index.js
+++ public/index.js
@@ -28,23 +28,47 @@ const displayCurrentWeather = function(weather) {
 
   const currentForecastTag = document.getElementById('current-weather-forecast');
 
+  // PLACE NAME
   const placeName = weather.city.name;
   const locationTag = document.createElement('h3');
   locationTag.innerText = placeName;
 
-  currentForecastTag.appendChild(locationTag);
-
+  // GET DATA
   const currentWeather = weather.list[0];
   const stats = currentWeather.main;
 
   const forecastTime = currentWeather.dt_txt;
+  const timeTag = document.createElement('h4');
+  timeTag.innerHTML = forecastTime;
+
   const weatherType = currentWeather.weather[0].main;
+  const weatherTypeTag = document.createElement('h4');
+  weatherTypeTag.innerText = weatherType;
+
   const temp = stats.temp;
   const pressure = stats.pressure;
   const humid = stats.humidity
   const clouds = currentWeather.clouds.all;
   const rain = currentWeather.rain['3h'];
-  const wind = currentWeather.wind;
+  const windSpeed = currentWeather.wind.speed;
+  const windDeg = currentWeather.wind.deg;
+
+  const statsArray = [temp, pressure, humid, clouds, rain, windSpeed, windDeg];
+  const statsLabels = ['Temp: ', "Pressure: ", "Humid: ", "Clouds: ", "Rain: ", "Wind Speed: ", "Wind Deg: "]
+
+  const ul = document.getElementById('stats-list');
+
+  for(let i = 0; i < statsArray.length; i++){
+    const li = document.createElement('li');
+    li.innerText = statsLabels[i] + statsArray[i];
+    ul.appendChild(li);
+  }
+
+  for (let element of [locationTag, timeTag, weatherTypeTag, ul]){
+    currentForecastTag.appendChild(element);
+  }
+
+  // append them
 
 }
 
@@ -67,6 +91,18 @@ var app = function(){
 
   const glasgowWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=3333231&APPID=d7d64ab41161dd5f312ccbe208418afe";
   makeRequest(glasgowWeatherURL, getWeatherData);
+
+  let date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  debugger;
+
+  const audioContext = new (window.AudioContext || window.webkitAudioContext);
+  var sine = audioContext.createOscillator();
+  sine.frequency.value = 100;
+  sine.start();
+  sine.connect(audioContext.destination);
+  // sine.stop(0.1);
 
 }
 
